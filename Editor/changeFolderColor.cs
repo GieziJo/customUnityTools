1c4a1cea4d385f874bac0a82aa13a76df481dea9
giezenda <jonathan.giezendanner@epfl.ch>
2018-07-04 16:04:20 +0200

    added an Editor method to color folders and sub-folders based on string

---
+++ Editor/changeFolderColor.cs
@@ -0,0 +1,62 @@
+﻿// ===============================
+// AUTHOR          : J. Giezendanner
+// CREATE DATE     : 04.07.2018
+// PURPOSE         : Add an overlay of a specific color ontop of specified folders in the Hierarchy window
+// SPECIAL NOTES   : inspired by Immanuel-Scholz's comment here: https://feedback.unity3d.com/suggestions/ability-to-change-the-folders-co 
+// ===============================
+// Change History:
+//
+//==================================
+
+using UnityEngine;
+using UnityEditor;
+
+
+[InitializeOnLoad]
+public class RecolorHierarchyWindow
+{
+
+    static RecolorHierarchyWindow()
+    {
+        EditorApplication.projectWindowItemOnGUI += RecolorLineFolder;
+    }
+    
+    static void RecolorLineFolder(string GUID, Rect rect)
+    {
+        if(Event.current.type == EventType.Repaint)
+        {
+            // Specify folders and colors to be affected here
+            if(setColorBaseForName(GUID, rect, "Scene", Color.yellow))
+                return;
+            if(setColorBaseForName(GUID, rect, "Script", Color.green))
+                return;
+            if(setColorBaseForName(GUID, rect, "Sprite", Color.red))
+                return;
+            if(setColorBaseForName(GUID, rect, "Prefab", new Color(.5f, 0f, 1f)))
+                return;
+        }
+    }
+    
+    static bool setColorBaseForName(string GUID,Rect rect, string topFolderName, Color folderColor)
+    {
+        string pth = AssetDatabase.GUIDToAssetPath(GUID);
+        if(pth.Contains(topFolderName))
+        {
+            
+            string[] pths = pth.Split("/"[0]);
+            string assetName = pths[pths.Length - 1];
+            
+            if(!assetName.Contains("."))
+            {
+                Texture2D texture = new Texture2D(1, 1);
+                Color c = new Color(folderColor.r,folderColor.g,folderColor.b,.1f);
+                texture.SetPixel(0,0,c);
+                texture.Apply();
+                GUI.skin.box.normal.background = texture;
+                GUI.Box(rect, GUIContent.none);
+                return true;
+            }
+        }
+        return false;
+    }
+}
